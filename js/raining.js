fa5e7028a75b5fb30cc875fbe5978cde9bf745ee
chiderlin <chiderlin36@gmail.com>
2021-06-08 09:24:04 +0800

    add obs_time

--- js/raining.js
+++ js/raining.js
@@ -1,4 +1,5 @@
 let records = null;
+let obs_time = null;
 let taiwan_cities = ["基隆市", "臺北市", "新北市", "桃園市", "新竹市", "新竹縣", "苗栗縣", "臺中市", "彰化縣", "南投縣", "雲林縣", "嘉義市", "嘉義縣", "臺南市", "高雄市", "屏東縣", "臺東縣", "花蓮縣", "宜蘭縣", "澎湖縣", "金門縣", "連江縣"];
 init();
 function init() {
@@ -13,6 +14,8 @@ function rainingAPI() {
 	}).then((data) => {
 		let len = data.records.location.length;
 		records = data.records;
+		obs_time = data.records.location[0].time.obsTime;
+		console.log(obs_time);		
 		for(let i=0; i<taiwan_cities.length; i++) {
 			let city = taiwan_cities[i];
 			findCityData(len, city);
@@ -25,6 +28,7 @@ function findCityData(len, current_city) {
 	for(let i=0; i<len; i++) {
 		const location = records.location[i];
 		let city = location.parameter[0].parameterValue
+		
 		if(city === current_city) {
 			let amount = location.weatherElement[6].elementValue;
 			if(amount === "-999.00") { // -999不知道是什麼雨量 => 歸零
@@ -44,7 +48,8 @@ function sumCityData(city_list, current_city) {
 	}
 	let data = {
 		"city": current_city,
-		"sum": sum
+		"sum": sum,
+		"obs_time": obs_time
 	}
 	renderCityRaining(data);
 };
@@ -52,36 +57,20 @@ function sumCityData(city_list, current_city) {
 
 
 function renderCityRaining(city_sum) {
+	console.log(city_sum);
 	const container = document.querySelector("#raining");
 	const item = document.createElement("div");
 	const city = document.createElement("div");
 	const amount = document.createElement("amount");
+	const obs_time = document.createElement("div");
 	item.className = "location";
 	city.className = "town";
 	amount.className = "amount";
 	city.textContent = city_sum.city;
 	amount.textContent = city_sum.sum + " mm";
+	obs_time.textContent = "Update : " + city_sum.obs_time;
 	container.appendChild(item);
 	item.appendChild(city);
 	item.appendChild(amount);
+	item.appendChild(obs_time);
 };
-
-// function renderRaining(page) {
-// 	let startIndex = page * 10;
-// 	let endIndex = (page + 1) * 10;
-// 	const container = document.querySelector("#raining");
-// 	for (let i = startIndex; i < endIndex; i++) {
-// 		const location = records.location[i];
-// 		const item = document.createElement("div");
-// 		const town = document.createElement("div");
-// 		const amount = document.createElement("amount");
-// 		item.className = "location";
-// 		town.className = "town";
-// 		amount.className = "amount";
-// 		town.textContent = location.parameter[0].parameterValue + "、" + location.parameter[2].parameterValue;
-// 		amount.textContent = location.weatherElement[6].elementValue + " mm";
-// 		item.appendChild(town);
-// 		item.appendChild(amount);
-// 		container.appendChild(item);
-// 	}
-// }
